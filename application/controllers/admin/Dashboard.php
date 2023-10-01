fac8643189b65bf1be041a962b8d3ecf1419f35d
nrzngr <109433343+nrzngr@users.noreply.github.com>
2023-10-01 21:34:19 +0700

    Appilcations folder containing MVC architecture

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+class Dashboard extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = "Dashboard";
+
+        // data pegawai (all)
+        $pegawai = $this->db->query("SELECT * FROM data_pegawai");
+        $data['pegawai'] = $pegawai->num_rows();
+
+        //data pegawai admin
+        $admin = $this->db->query("SELECT * FROM data_pegawai WHERE jabatan ='Admin'");
+        $data['admin'] = $admin->num_rows();
+
+        $jabatan = $this->db->query("SELECT * FROM data_jabatan");
+        $data['jabatan'] = $jabatan->num_rows();
+
+        $kehadiran = $this->db->query("SELECT * FROM data_kehadiran");
+        $data['kehadiran'] = $kehadiran->num_rows();
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/dashboard', $data);
+        $this->load->view('templates_admin/footer');
+
+    }
+}
+
+?>
